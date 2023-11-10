b9257f74b7811efaddae4295c48b5336cb217d6a
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2023-11-10 12:15:21 +0200

    Check if the given array is ordered in descending order.

--- src/ua/matveenko/hw5/Task2.java
+++ src/ua/matveenko/hw5/Task2.java
@@ -2,13 +2,31 @@ package ua.matveenko.hw5;
 
 import java.util.Arrays;
 
+
 public class Task2 {
     public static void main(String[] args) {
-        int[] numbers = {10, 25, 6, 5, 3, 2, 1, 0};
-        System.out.println(Arrays.toString(numbers));
-        boolean result =checkDescending(numbers);
+        int[][] numbers = {{10, 9, 8, 7}, {5, 4, 3, 13}};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(Arrays.toString(numbers[i]));
+        }
+        int[] array = new int[numbers[0].length * numbers.length];
+        copyTwoToOneArray(numbers, array);
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(Arrays.toString(numbers[i]));
+        }
+        boolean result = checkDescending(array);
         System.out.println("descending check: " + result);
-        System.out.println(Arrays.toString(numbers));
+    }
+
+
+    public static void copyTwoToOneArray(int[][] numbers, int[] array) {
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[0].length; j++) {
+                array[count++] = numbers[i][j];
+            }
+        }
     }
 
     public static boolean checkDescending(int[] array) {
@@ -24,3 +42,8 @@ public class Task2 {
         return result;
     }
 }
+
+
+
+
+
