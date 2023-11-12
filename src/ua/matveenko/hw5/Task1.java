477fbab3f5131b96cbcae6839c9b0c0215d039ba
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2023-11-12 17:05:53 +0200

    corrected

--- src/ua/matveenko/hw5/Task1.java
+++ src/ua/matveenko/hw5/Task1.java
@@ -5,34 +5,26 @@ import java.util.Arrays;
 public class Task1 {
     public static void main(String[] args) {
         int[][] numbers = new int[3][4];
-        arrangeElements(numbers);
         moduloTheElementsOfOddRows(numbers);
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(Arrays.toString(numbers[i]));
         }
     }
 
-
-    public static void arrangeElements(int[][] array) {
-        int k = -1;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = k;
-                k--;
-            }
-        }
-    }
-
     public static void moduloTheElementsOfOddRows(int[][] array) {
+        int k = 1;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (i % 2 == 0) {
-                    array[i][j] = Math.abs(array[i][j]);
+                    array[i][j] = k;
+                    k++;
+                } else {
+                    array[i][j] = -(k);
+                    k++;
                 }
             }
         }
     }
+
 }
-
-
 
