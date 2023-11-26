66fc7341edc38a440e798844347118af794d1cbf
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2023-11-27 00:04:04 +0200

    corrected

--- src/ua/matveenko/hw10/PhoneBook.java
+++ src/ua/matveenko/hw10/PhoneBook.java
@@ -1,5 +1,6 @@
 package ua.matveenko.hw10;
 import java.lang.String;
+import java.util.Objects;
 import java.util.Optional;
 
 class PhoneBook {
@@ -9,7 +10,7 @@ class PhoneBook {
         PHONE_BOOK[0] = "016/161616";
         PHONE_BOOK[1] = "016/161617";
         PHONE_BOOK[2] = "016/161618";
-        Optional<Integer> optional = findIndexByPhoneNumber("016/161616");
+        Optional<Integer> optional = findIndexByPhoneNumber("016/16161");
         if (optional.isPresent()) {
             System.out.println(optional.get());
         } else {
@@ -20,7 +21,7 @@ class PhoneBook {
 
     public static Optional<Integer> findIndexByPhoneNumber(String phoneNumber) {
         for (int i = 0; i < PHONE_BOOK.length; i++) {
-            if (PHONE_BOOK[i] != null && PHONE_BOOK[i].equals(phoneNumber)) {
+            if (Objects.equals(PHONE_BOOK[i],phoneNumber)) {
                 return Optional.of(i);
             }
         }
